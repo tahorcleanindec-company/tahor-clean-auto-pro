d59942ac14120de0202eab909c0b90f5027ec316
Jossg <tahorclean.ind.ec@gmail.com>
2026-04-24 08:19:35 -0500

    Actualizar estilos y scripts

--- app.js
+++ app.js
@@ -24,24 +24,9 @@ const products = [
     rating: 4.8,
     reviews: 124
   },
-  {
-    id: 2,
-    name: "Shampoo EC",
-    description: "Shampoo automotriz formulado para limpieza diaria, elimina suciedad ligera sin afectar pintura ni acabados. pH 7.0",
-    category: "Lavado",
-    images: [
-      "img/SHAMPOO PARA AUTO ECONOMICO 20L.webp",
-    ],
-    video: "",
-    features: ["Limpieza suave y segura ", "No daña pintura ni plásticos ", "Ideal para uso frecuente ", "Fácil enjuague", "Uso doméstico y profesional"],
-    sizes: [
-      { name: "Caneca 20", code: "63506", price: 15, icon: "🛢️", iva: 0.15 },
-      { name: "Caneca 28", code: "63226", price: 19, icon: "🛢️", iva: 0.15 }
+  
+ 
 
-    ],
-    rating: 4.9,
-    reviews: 89
-  },
   { 
     id: 3,
     name: "Shampoo con Cera",
---
+++ t.js
@@ -0,0 +1,18 @@
+ {
+    id: 2,
+    name: "Shampoo EC",
+    description: "Shampoo automotriz formulado para limpieza diaria, elimina suciedad ligera sin afectar pintura ni acabados. pH 7.0",
+    category: "Lavado",
+    images: [
+      "img/SHAMPOO PARA AUTO ECONOMICO 20L.webp",
+    ],
+    video: "",
+    features: ["Limpieza suave y segura ", "No daña pintura ni plásticos ", "Ideal para uso frecuente ", "Fácil enjuague", "Uso doméstico y profesional"],
+    sizes: [
+      { name: "Caneca 20", code: "63506", price: 15, icon: "🛢️", iva: 0.15 },
+      { name: "Caneca 28", code: "63226", price: 19, icon: "🛢️", iva: 0.15 }
+
+    ],
+    rating: 4.9,
+    reviews: 89
+  },
